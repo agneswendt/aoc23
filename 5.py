15130c0cfd30c610cf31313181ca524565a1cfab
Agnes Wendt <agnwe839@student.liu.se>
2023-12-05 15:02:46 +0100

    👵🏻

--- 5.py
+++ 5.py
@@ -1,6 +1,5 @@
 import time
 from aocd.models import Puzzle
-from functools import cache
 
 
 day, year = 5, 2023
@@ -9,39 +8,7 @@ day, year = 5, 2023
 def get_input():
     puzzle = Puzzle(year, day)
     res = []
-    data = """seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4"""
+    data = """"""
     data = puzzle.input_data
     seeds = ""
     res = {}
@@ -144,8 +111,6 @@ def p2(data):
             )
             first = None
     return res
-
-    # return min(res)
 
 
 if __name__ == "__main__":
